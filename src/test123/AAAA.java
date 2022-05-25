373e59a8ad90aef8ba8eedcff9697ba5128d452d
zhou_pro <2293638217@qq.com>
2022-05-25 09:05:32 +0800

    Initial commit

---
+++ src/test123/AAAA.java
@@ -0,0 +1,11 @@
+package test123;
+
+
+
+public class AAAA {
+    public static void main(String[] args) {
+        System.out.println(123123);
+    }
+
+
+}
---
+++ src/test123/Test.java
@@ -0,0 +1,14 @@
+package test123;
+
+public class Test {
+	public static void main(String[] args) {
+		System.out.println(123);
+
+
+
+		
+		
+	}
+	
+	
+}
